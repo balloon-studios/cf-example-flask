542ebaff6c587ef051687cc3d8a0908805631997
Jonathan Matthews <contact@jpluscplusm.com>
2015-12-18 00:21:25 +0000

    Show environment data

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
+import pprint
 from flask import Flask, Response, request
+import logging
 
 app = Flask(__name__)
 
+logging.basicConfig(level='DEBUG')
+log = logging.getLogger()
+
 @app.route('/')
 def hello():
-    str = "Hello, world!"
-    return Response(str, mimetype="text/html")
+    info = { "Request": request.environ, "Environment": os.environ.__dict__['data'] }
+    str = pprint.pformat(info,width=10)
+    return Response(str, mimetype="text/plain")
 
 
 if __name__ == "__main__":
